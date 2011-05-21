1df4e46ef23e64186b2371712f2be914b4ba4a5c
unknown <juand@.telenav.com>
2011-05-20 17:41:57 -0700

    session fixed

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,27 +1,33 @@
 module SessionsHelper
 
   def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-    current_user = user
+    #cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    session[:user_id] = [user.id, user.salt]
+    self.current_user = user
   end
-
-  def sign_out
-    cookies.delete(:remember_token)
-    current_user = nil
-  end
-
+ 
   def current_user=(user)
     @current_user = user
   end
-
+ 
   def current_user
     @current_user ||= user_from_remember_token
   end
-
+  
+  def sign_out
+    #cookies.delete(:remember_token)
+    session[:user_id] = nil
+    self.current_user = nil
+  end
+  
   def signed_in?
     !current_user.nil?
   end
-
+  
+  def current_user?(user)
+    current_user == user
+  end
+  
   def deny_access
     store_location
     redirect_to signin_path, :notice => "Please sign in to access this page."
@@ -31,26 +37,23 @@ module SessionsHelper
     redirect_to(session[:return_to] || default)
     clear_return_to
   end
-
-  def current_user?(user)
-    user == current_user
+  
+private
+  
+  def user_from_remember_token
+    User.authenticate_with_salt(*remember_token)
   end
-
-  private
   
-    def user_from_remember_token
-      User.authenticate_with_salt(*remember_token)
-    end
+  def remember_token
+    #cookies.signed[:remember_token] || [nil, nil]
+    session[:user_id] || [nil, nil]
+  end
   
-    def remember_token
-      cookies.signed[:remember_token] || [nil, nil]
-    end
-
-    def store_location
-      session[:return_to] = request.fullpath
-    end
-
-    def clear_return_to
-      session[:return_to] = nil
-    end
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+  
+  def clear_return_to
+    session[:return_to] = nil
+  end
 end
