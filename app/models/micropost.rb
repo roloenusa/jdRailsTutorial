29f7a2b81d7d2d10e64ab93bf4871ed39d5afa4e
roloenusa <roloenusa@msn.com>
2011-07-13 19:15:17 -0700

    Rails Tutorial Chapter 11.1 Complete

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,14 @@
 class Micropost < ActiveRecord::Base
+  attr_accessible :content
+  
+  validates :content, :presence => true, :length => { :maximum => 140 }
+  validates :user_id, :presence => true
+  
+  belongs_to :user
+  
+  # The tutorial contains the following
+  # default_scope :order => 'microposts.created_at DESC'
+  # this doesn't seem to work. A quick google resolved to a working solution:
+  default_scope order('microposts.created_at DESC') 
+  
 end
