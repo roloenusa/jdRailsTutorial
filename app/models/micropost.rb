36a4cd19a2c240f11ff3823ac8518d3cd7a1a1e4
roloenusa <roloenusa@msn.com>
2011-07-22 18:22:30 -0700

    Rails Tutorial Chapter 12 Completed.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,10 +7,25 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   
   # The tutorial contains the following
-  # default_scope :order => 'microposts.created_at DESC'
+  default_scope :order => 'microposts.created_at DESC'
   # this doesn't seem to work. A quick google resolved to a working solution:
-  default_scope order('microposts.created_at DESC') 
+  #default_scope order('microposts.created_at DESC') 
   
+  # return microposts from the users being followed by the given user.
+  scope :from_users_followed_by, lambda { |user| followed_by (user) }
+  
+  
+  # def self.from_users_followed_by(user)
+  #   where(:user_id => user.following.push(user))
+  # end
+  
+private 
+  # return an SQL condition for users followed by the given user.
+  # we include the user's own id as well
+  def self.followed_by(user)
+    following_ids = %(SELECT followed_id FROM relationships WHERE follower_id = :user_id)
+    where("user_id IN (#{following_ids}) or user_id = :user_id", { :user_id => user})
+  end
 end
 
 # == Schema Information
